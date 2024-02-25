b889601b6d30c36f48995b26924744f8cc7bfd08
John Uroko Raspberry <jburoko@gmail.com>
2024-02-25 11:55:52 +0100

    Added parser clsses and split video recording file

---
+++ examples/CPDetector/utils/camera_settings.py
@@ -0,0 +1,97 @@
+import depthai as dai
+from utils.parser import ENVParser, CMDParser
+
+
+class CameraSettings:
+    def __init__(self):
+        env_parser = ENVParser()
+        cmd_parser = CMDParser()
+
+        # Weights to use when blending depth/rgb image (should equal 1.0)
+        self.rgbWeight = env_parser.rgb_weight
+        self.depthWeight = env_parser.depth_weight
+
+        # Camera parameters
+        self.fps = env_parser.fps
+        self.video_width = env_parser.video_width
+        self.video_height = env_parser.video_height
+        self.alpha = cmd_parser.alpha
+
+        # The disparity is computed at this resolution, then upscaled to RGB resolution
+        self.monoResolution = dai.MonoCameraProperties.SensorResolution.THE_720_P
+
+        # Properties
+        self.rgbCamSocket = dai.CameraBoardSocket.CAM_A
+
+        # Pipeline Parameters
+        self._pipeline = dai.Pipeline()
+        self._device = dai.Device()
+        self._stereo = None
+    
+    @property
+    def pipeline(self):
+        return self._pipeline
+    
+    @property
+    def device(self):
+        return self._device
+    
+    @property
+    def stereo(self):
+        return self._stereo
+
+    def setup_pipeline(self):
+        # Create pipeline
+        queueNames = []
+
+        # Define sources and outputs
+        camRgb = self._pipeline.create(dai.node.Camera)
+        left = self._pipeline.create(dai.node.MonoCamera)
+        right = self._pipeline.create(dai.node.MonoCamera)
+        stereo = self._pipeline.create(dai.node.StereoDepth)
+
+        rgbOut = self._pipeline.create(dai.node.XLinkOut)
+        disparityOut = self._pipeline.create(dai.node.XLinkOut)
+
+        rgbOut.setStreamName("rgb")
+        queueNames.append("rgb")
+        disparityOut.setStreamName("disp")
+        queueNames.append("disp")
+
+        camRgb.setBoardSocket(self.rgbCamSocket)
+        camRgb.setSize(self.video_width, self.video_height)
+        camRgb.setFps(self.fps)
+
+        # For now, RGB needs fixed focus to properly align with depth.
+        # This value was used during calibration
+        try:
+            calibData = self._device.readCalibration2()
+            lensPosition = calibData.getLensPosition(self.rgbCamSocket)
+            if lensPosition:
+                camRgb.initialControl.setManualFocus(lensPosition)
+        except:
+            raise
+        left.setResolution(self.monoResolution)
+        left.setCamera("left")
+        left.setFps(self.fps)
+        right.setResolution(self.monoResolution)
+        right.setCamera("right")
+        right.setFps(self.fps)
+
+        stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+        # LR-check is required for depth alignment
+        stereo.setLeftRightCheck(True)
+        stereo.setDepthAlign(self.rgbCamSocket)
+
+        # Linking
+        camRgb.video.link(rgbOut.input)
+        left.out.link(stereo.left)
+        right.out.link(stereo.right)
+        stereo.disparity.link(disparityOut.input)
+
+        camRgb.setMeshSource(dai.CameraProperties.WarpMeshSource.CALIBRATION)
+        if self.alpha is not None:
+            camRgb.setCalibrationAlpha(self.alpha)
+            stereo.setAlphaScaling(self.alpha)
+        
+        self._stereo = stereo
---
+++ examples/CPDetector/utils/parser.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+from argparse import ArgumentParser
+
+class ENVParser:
+    def __init__(self):
+        load_dotenv(dotenv_path="examples/CPDetector/.env")
+        self._rgb_weight = os.getenv("RGB_WEIGHT")
+        self._depth_weight = os.getenv("DEPTH_WEIGHT")
+        self._fps = os.getenv("FPS")
+        self._video_width = os.getenv("VIDEO_WIDTH")
+        self._video_height = os.getenv("VIDEO_HEIGHT")
+    
+    @property
+    def rgb_weight(self):
+        return float(self._rgb_weight)
+    
+    @property
+    def depth_weight(self):
+        return float(self._depth_weight)
+    
+    @property
+    def fps(self):
+        return int(self._fps)
+    
+    @property
+    def video_width(self):
+        return int(self._video_width)
+    
+    @property
+    def video_height(self):
+        return int(self._video_height)
+
+class CMDParser:
+    def __init__(self):
+        parser = ArgumentParser()
+        parser.add_argument('-alpha', type=float, default=None, help="Alpha scaling parameter to increase float. [0,1] valid interval.")
+        
+        args = parser.parse_args()
+        self._alpha = args.alpha
+
+    @property
+    def alpha(self):
+        return self._alpha
