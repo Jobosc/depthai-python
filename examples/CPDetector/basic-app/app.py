4e875cd8631edd6a2df54c58fca41c851b520eab
John Uroko Raspberry <Jburoko@gmail.com>
2024-05-05 11:11:14 +0200

    Add video recording to frontend

--- examples/CPDetector/basic-app/app.py
+++ examples/CPDetector/basic-app/app.py
@@ -2,14 +2,25 @@ from shiny import reactive
 from shiny.express import input, render, ui
 
 from participant import Participant
+import faicons as fa
+import datetime
+from video_recording import run
 
+ICONS = {
+    "user": fa.icon_svg("user", "regular"),
+    "calendar": fa.icon_svg("calendar"),
+    "gear": fa.icon_svg("gear"),
+}
 
+camera_running = reactive.value(True)
+
+ui.page_opts(title="Gait Recording", fillable=True)
 #######################################################
 #                     Sidebar
 #######################################################
 
 with ui.sidebar(id="sidebar"):
-    "Metadata"
+    "Input data"
 
     ui.input_dark_mode(id="mode")
 
@@ -25,46 +36,67 @@ with ui.sidebar(id="sidebar"):
 
     # Action Button
     ui.input_action_button("save_button", "Save")
-    ui.input_action_button("reset_button", "Reset (?)")
 
-    # Metadata to save
-    @render.text(False)
-    def text_out():
-        result = ""
-        if input.name() != "":
-            result = result + f"Name: {input.name()}" + "\n"
-        if input.subjects() != "":
-            result = result + f"Subjects: {input.subjects()} \n"
-        if input.grade() != "":
-            result = result + f"Grade: {input.grade()}\n"
-        if input.gender() != "":
-            result = result + f"Gender: {input.gender()}"
+    with ui.panel_conditional(
+        "input.name || input.subjects || input.grade || input.gender"
+    ):
+        ui.input_action_button("reset_button", "Reset")
 
-        return result
+        with ui.value_box(showcase=ICONS["user"]):
+            """"""
+            "Metadata"
+
+            @render.express
+            def metadata_output():
+                result = ""
+                if input.name() != "":
+                    result = result + f"Name: {input.name()}" + "\n"
+                if input.subjects() != "":
+                    result = result + f"Subjects: {input.subjects()} \n"
+                if input.grade() != "":
+                    result = result + f"Grade: {input.grade()}\n"
+                if input.gender() != "":
+                    result = result + f"Gender: {input.gender()}"
+                result
 
 
 #######################################################
-#                     Main View
+#                   Main View
 #######################################################
 
-ui.panel_title("Gait Recorder")
+ui.panel_title("Videos")
 
-metadata = reactive.value("0")
+with ui.layout_columns(fill=False):
+
+    with ui.value_box(showcase=ICONS["user"]):
+        "Recorded users (?)"
+
+        @render.express
+        def recorded_user():
+            pass
+
+    with ui.value_box(showcase=ICONS["calendar"]):
+        "Date"
+        date = datetime.datetime.now().strftime("%d-%m-%Y")
+        date
 
 
 ui.input_slider("n", "N", 0, 100, 20)
+
+
+# Action Button
+ui.input_action_button("record_button", "Start recording")
+
+# with ui.card():
+#    ui.markdown(result)
 
 
 def my_slider(id):
     return ui.input_slider(id, "N", 0, 100, 20)
 
 
-# with ui.card():
-#    ui.markdown(result)
-
-
 #######################################################
-#                     Events
+#                    Events
 #######################################################
 
 
@@ -80,15 +112,18 @@ def store_metadata():
     return f"{input.save_button()}"
 
 
-@render.text
+@reactive.effect
 @reactive.event(input.reset_button)
 def reset_metadata():
-    pass  # input.name = ""
+    ui.update_text("name", value=f"")
+    ui.update_text("subjects", value=f"")
+    ui.update_text("grade", value=f"")
+    ui.update_text("gender", value=f"")
+    ui.update_text("comments", value=f"")
 
 
-# @reactive.event(input.name)
-@render.text
-def name_metadata():
-    if isinstance(input.name(), str):
-        # result = result + f"**Name**: {input.name()}"
-        pass  # metadata.set(input.name())
+@reactive.effect
+@reactive.event(input.record_button)
+def start_recording():
+    run()
+    ui.update_action_button("record_button", label=f"Stop recording")
