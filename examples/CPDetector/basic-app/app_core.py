faa60a6cc1d695863abdfd050b60c8a4977398ea
John Raspberry Pi <Jburoko@gmail.com>
2024-11-24 12:43:20 +0100

    Fi recording with depthai API

--- examples/CPDetector/basic-app/app_core.py
+++ examples/CPDetector/basic-app/app_core.py
@@ -79,3 +79,5 @@ def server(input: Inputs, output: Outputs, session: Session):
 
 
 app = App(app_ui, server, static_assets={f"/{env.temp_path}": os.path.join(env.main_path, env.temp_path)})
+
+#Save files from any day. Not just today
--- examples/CPDetector/basic-app/features/modules/camera.py
+++ examples/CPDetector/basic-app/features/modules/camera.py
@@ -17,7 +17,6 @@ from datetime import datetime, timedelta
 import cv2
 import depthai as dai
 import numpy as np
-from depthai_sdk import OakCamera
 
 from features.modules.light_barrier import LightBarrier
 from features.modules.time_window import TimeWindow
@@ -49,7 +48,7 @@ class Camera(object):
         if cls._instance is None:
             logging.debug("Initiate camera instance.")
             cls._instance = super(Camera, cls).__new__(cls)
-            cls.encode = dai.VideoEncoderProperties.Profile.MJPEG
+            cls.encode = dai.VideoEncoderProperties.Profile.H265_MAIN
             cls.fps = 40
         return cls._instance
 
@@ -147,8 +146,9 @@ class Camera(object):
 
                 # Open a file to save encoded video
                 day = datetime.now().strftime(env.date_format)
-                with open(os.path.join(env.temp_path, day, "color.mjpeg"), 'wb') as video_file, open(
-                        os.path.join(env.temp_path, day, "disparity.mjpeg"), 'wb') as depth_file:
+                os.makedirs(os.path.join(env.temp_path, day), exist_ok=True)
+                with open(os.path.join(env.temp_path, day, "color.mp4"), 'wb') as video_file, open(
+                        os.path.join(env.temp_path, day, "disparity.mp4"), 'wb') as depth_file:
                     print("Press Ctrl+C to stop encoding...")
                     while True:
                         try:
@@ -205,82 +205,12 @@ class Camera(object):
 
             return 1
 
-        with OakCamera() as oak:
-            """# Define cameras
-            oak.device.setTimesync(timedelta(seconds=1), 10, True)
-
-            color = oak.camera(
-                source=dai.CameraBoardSocket.CAM_A,
-                resolution=dai.ColorCameraProperties.SensorResolution.THE_1080_P,
-                fps=self.fps,
-                encode=self.encode if self.mode else None,
-            )
-            stereo = oak.stereo(
-                resolution=dai.MonoCameraProperties.SensorResolution.THE_800_P,
-                fps=self.fps,
-                encode=self.encode,
-            )"""
-
-            # Set IR brightness
+            """# Set IR brightness
             # Turned off IR because of the possible marker disruptions
             stereo.set_auto_ir(auto_mode=False, continuous_mode=False)
             stereo.set_ir(0, 0)
-            logging.info("Set camera parameters for recording with OAK camera.")
+            logging.info("Set camera parameters for recording with OAK camera.")"""
 
-            """# Synchronize & save all (encoded) streams
-            if self.mode:
-                logging.info("Record video without stream.")
-                # Folder parameters
-                day = datetime.now().strftime(env.date_format)
-
-                oak.record(
-                    [color.out.encoded, stereo.out.encoded],
-                    os.path.join(env.temp_path, day),
-                    RecordType.VIDEO,
-                )
-            else:
-                logging.info("View video without recording.")
-                oak.visualize([color.out.camera, stereo.out.depth], fps=True, scale=1 / 2)
-
-            oak.start(blocking=block)
-            logging.info(f"Camera started recording at: {datetime.now()}")
-
-            timestamps.camera_start = datetime.now()
-            current_state = 0
-            startpoint = None"""
-            """while oak.running():
-                time.sleep(0.001)
-                oak.poll()
-
-                if not self.ready:  # not state.activated or
-                    oak.device.close()
-                    cv2.destroyAllWindows()
-
-                    if startpoint is not None:
-                        endpoint = datetime.now()
-                        logging.info(f"Light barrier triggered to end at: {endpoint}")
-                        if endpoint - startpoint > timedelta(seconds=2):
-                            timestamps.time_windows.append(TimeWindow(start=startpoint, end=endpoint))
-
-                try:
-                    if current_state != state.activated:
-                        if state.activated:
-                            startpoint = datetime.now()
-                            logging.info(f"Light barrier triggered to start at: {startpoint}")
-                        else:
-                            endpoint = datetime.now()
-                            logging.info(f"Light barrier triggered to end at: {endpoint}")
-                            if endpoint - startpoint > timedelta(seconds=2):
-                                timestamps.time_windows.append(TimeWindow(start=startpoint, end=endpoint))
-                            startpoint = None
-                            endpoint = None
-                    current_state = state.activated
-                except:
-                    logging.warning("There was an issue storing a time point.")
-
-            self.running = False
-
-            return 1"""
 
     @property
     def camera_connection(self) -> bool:
