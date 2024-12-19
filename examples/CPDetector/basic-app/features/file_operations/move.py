77e858e6b6609a3e70129527aec6f9b8208ea8d0
John Bosco Uroko <jburoko@gmail.com>
2024-12-19 13:48:33 +0100

    Add multiprocessing to move files

--- examples/CPDetector/basic-app/features/file_operations/move.py
+++ examples/CPDetector/basic-app/features/file_operations/move.py
@@ -9,10 +9,10 @@ Functions:
 """
 
 import shutil
+from multiprocessing import Pool, cpu_count
 from typing import List
 
 from features.file_operations.delete import delete_temporary_recordings
-from features.file_operations.video_processing import convert_npy_files_to_video
 from features.modules.participant import Participant
 from . import os, today, temporary_path, logging, storage_path
 
@@ -44,20 +44,26 @@ def move_data_from_temp_to_main_storage(folder_id: str, participant: Participant
     destination_path = os.path.join(storage_path, day, folder_id)
     os.makedirs(destination_path, exist_ok=True)
 
-    # Create the disparity video before moving all files
-    #convert_npy_files_to_video(os.path.join(temporary_path, day, "depth_frames"), "depth.mp4", True)
-    #convert_npy_files_to_video(os.path.join(temporary_path, day, "rgb_frames"), "rgb.mp4", False)
-
+    files_to_move = []
     for root, directory, files in os.walk(os.path.join(temporary_path, day)):
         for dir in directory:
             os.makedirs(os.path.join(destination_path, dir), exist_ok=True)
         for file in files:
             norm_path = os.path.normpath(root)
             folder = os.path.basename(norm_path)
-            shutil.copy2(os.path.join(root, file), os.path.join(destination_path, folder, file))  # Copy file
-            logging.debug(
-                f"Moving file: {os.path.join(root, file)} to {os.path.join(destination_path, folder, file)} complete.")
-            yield True
-    participant.store_participant_metadata(os.path.join(storage_path, day, folder_id))
+            src_file = os.path.join(root, file)
+            dest_file = os.path.join(destination_path, folder, file)
+            files_to_move.append((src_file, dest_file))
 
+    with Pool(processes=cpu_count()) as pool:
+        for _ in pool.imap(__move_file, files_to_move):
+            yield True
+
+    participant.store_participant_metadata(os.path.join(storage_path, day, folder_id))
     delete_temporary_recordings()
+
+
+def __move_file(args: tuple) -> None:
+    src_file, dest_file = args
+    shutil.copy2(src_file, dest_file)
+    logging.debug(f"Moved file: {src_file} to {dest_file}")
